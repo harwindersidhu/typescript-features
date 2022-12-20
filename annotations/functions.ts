ef7439db93cb37f356bfafcd0be9f4a26bf7770d
Harwinder Singh Sidhu <harwindersidhu66@gmail.com>
2022-12-20 02:27:28 -0700

    Annotations around functions

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -4,4 +4,38 @@ const add = (a: number, b: number): number => {
 
 const subtract = (a: number, b: number): number => {
   return a-b;
+};
+
+function divide(a: number, b: number): number {
+  return a/b;
 }
+
+const multiply = function(a: number, b: number): number {
+  return a*b;
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+const throwErrorSecondExample = (message: string): void => {
+  if (!message) {
+    throw new Error(message);
+  }
+};
+
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny'
+};
+
+const logWeather = (forecast: { date: Date, weather: string}): void => {
+  console.log(forecast.date);
+  console.log(forecast.weather);
+};
+
+logWeather(todaysWeather);
